0652dd3fa5683eb6da66e38d85bd423dcaaf0cd8
Peter Taggart <staggarts@gmail.com>
2018-10-28 00:28:13 -0600

    update version in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pywit',
-    version='1.0.3',
+    version='1.1.0',
     description='Steem Python Witness Toolkit',
     packages=find_packages(),
     setup_requires=['pytest-runner'],
